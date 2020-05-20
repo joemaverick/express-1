e5cbf77e26565ed564e533acda2e3e802d532a13
Joseph Yehulugeta <40561571+joemaverick@users.noreply.github.com>
2020-05-20 11:17:26 -0700

    import data

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var data = require('./data/test.json');
 var app = express();
 const PORT = 3000;
 
@@ -8,4 +9,5 @@ app.use(express.static(__dirname));
 // app.listen(3000);
 app.listen(PORT,()=>{
   console.log(`App is running on port ${PORT}`);
+  console.log(data)
 });
