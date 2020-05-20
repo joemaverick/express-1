e2ac2eaed3e9a8862c8edbb66b9b712056b46905
Joseph Yehulugeta <40561571+joemaverick@users.noreply.github.com>
2020-05-20 11:00:18 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 var express = require('express');
 var app = express();
+const PORT = 3000;
+
 
 //we can now server up static pages
 app.use(express.static(__dirname));
-app.listen(3000);
-
+// app.listen(3000);
+app.listen(PORT,()=>{
+  console.log(`App is running on port ${PORT}`);
+});
