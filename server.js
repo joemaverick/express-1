48c9fbb824c53d5ff3ea2951eac08bf9e0b74b2c
Joseph Yehulugeta <40561571+joemaverick@users.noreply.github.com>
2020-05-27 09:29:41 -0700

    Update server.js

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ const PORT = 3000;
 
 //this is how a route is built in Express
 app.get('/',(req,res)=>{
-  res.send(`App is running on port ${PORT}`);
-
+//   res.send(`App is running on port ${PORT}`);
+  res.json(data);
 });
 
 
